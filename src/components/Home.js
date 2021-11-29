85816e871f6d4566b55a5f6231037baed1d6fa87
Yusuf SEVİLGEN <Yusufsevilgenonline@gmail.com>
2021-11-29 14:27:35 +0300

    react router start

---
+++ src/components/Home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Home = () => {
+    return(
+        <div 
+            className='ui raised very padded text container segment'
+            style= {{marginTop: '80px'}}
+        >
+            <h3 className='ui header'> 
+                Home
+            </h3>
+            <p>
+                Lorem Ipsum Nedir?
+                Ipsum, dizgi ve baskı endüstrisinde kullanılan 
+                mıgır metinlerdir. Lorem Ipsum, adı bilinmeyen 
+                bir matbaacının bir hurufat numune kitabı oluşturmak 
+                üzere bir yazı galerisini alarak karıştırdığı 1500'lerden 
+                beri endüstri standardı sahte metinler olarak kullanılmıştır. Beşyüz yıl boyunca varlığını sürdürmekle kalmamış, aynı zamanda pek değişmeden elektronik dizgiye de sıçramıştır. 1960'larda Lorem Ipsum pasajları da içeren Letraset yapraklarının yayınlanması ile ve yakın zamanda Aldus PageMaker gibi Lorem Ipsum sürümleri içeren masaüstü yayıncılık yazılımları ile popüler olmuştur. 
+            </p>
+        </div>
+    )
+}
+
+export default Home; 
